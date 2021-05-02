8c426dc6f8a58050faa21f1f1e0e125b0d5fadf6
shankeyraheja <shankeyraheja@gmail.com>
2021-05-02 12:54:32 +1200

    Final Backend

--- server.js
+++ server.js
@@ -23,19 +23,33 @@ app.post("/data", async (req,res) => {
   var data = await fetch(api_url)
   var data2 = await data.json()
   res.json([data2.foods[0].fdcId, data2.foods[0].description, data2.foods[0].foodNutrients])
+  // .foods[0].fdcId, data2.foods[0].description, data2.foods[0].foodNutrients
 })
-
+app.post("/data", async (req,res) => {
+  let params = {
+    api_key: "gBVxdic66dh8BJmcU9hZKBanpfqCdBeIoJm3x6Xi",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 1,
+    query: req.body.search
+  }
+  let api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&dataType=${encodeURIComponent(params.dataType)}&pageSize=${encodeURIComponent(params.pagesize)}&query=${encodeURIComponent(params.query)}`
+  var data = await fetch(api_url)
+  var data2 = await data.json()
+  res.json([data2.foods[0].fdcId, data2.foods[0].description, data2.foods[0].foodNutrients])
+})
 
 
 app.post("/list", async (req,res) => {
   let params = {
     api_key: "gBVxdic66dh8BJmcU9hZKBanpfqCdBeIoJm3x6Xi",
     dataType: ["Survey (FNDDS)"],
-    pagesize: 15,
-    query: ""
+    pagesize: 500,
+    query: req.body.search
   }
   let api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&dataType=${encodeURIComponent(params.dataType)}&pageSize=${encodeURIComponent(params.pagesize)}&query=${encodeURIComponent(params.query)}`
+  // &pageSize=${encodeURIComponent(params.pagesize)}
   var data = await fetch(api_url)
+
   var data2 = await data.json()
   list = []
   for(let i=0;i<data2.foods.length;i++){
